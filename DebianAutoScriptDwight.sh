c49af6e2d2ab2f0a4df5c0c2d8bfd5b2bb3d30c4
GwapoDwight <40294801+GwapoDwight@users.noreply.github.com>
2018-06-19 08:53:20 +0800

    Update DebianAutoScriptDwight.sh

--- DebianAutoScriptDwight.sh
+++ DebianAutoScriptDwight.sh
@@ -149,8 +149,8 @@ iptables -A INPUT -p udp -m string --algo bm --string "tracker" -j DROP
 iptables -I INPUT -p udp -m string --algo bm --string "BitTorrent" -j DROP 
 iptables -I INPUT -p udp -m string --algo bm --string "BitTorrent protocol" -j DROP iptables -I INPUT -p udp -m string --algo bm --string "peer_id=" -j DROP 
 iptables -I INPUT -p udp -m string --algo bm --string ".torrent" -j DROP 
-iptables -I INPUT -p udp -m string --algo bm --string "announce.php?passkey=" -j DROP iptables -I INPUT -p udp -m string --algo bm --string "torrent" -j DROP i
-ptables -I INPUT -p udp -m string --algo bm --string "announce" -j DROP
+iptables -I INPUT -p udp -m string --algo bm --string "announce.php?passkey=" -j DROP iptables -I INPUT -p udp -m string --algo bm --string "torrent" -j DROP 
+iptables -I INPUT -p udp -m string --algo bm --string "announce" -j DROP
 iptables -I INPUT -p udp -m string --algo bm --string "info_hash" -j DROP 
 iptables -I INPUT -p udp -m string --algo bm --string "tracker" -j DROP 
 iptables -D INPUT -p udp -m string --algo bm --string "BitTorrent" -j DROP 
